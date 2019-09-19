41ec386969633281c79a3ed6d2a7eaf63b28edd9
lumotian <lumotian@baidu.com>
2019-09-20 04:57:29 +0800

    aa

--- render.js
+++ render.js
@@ -1,11 +1,13 @@
 const BaseBot = require('bot-sdk');
 const fs = require('fs');
 
+const Document = BaseBot.Directive.DPL.Document
+const RenderDocument = BaseBot.Directive.DPL.RenderDocument
 
 module.exports = function render() {
     const doc = JSON.parse(fs.readFileSync('./launchDPL.json').toString())
-    const document = new Bot.Directive.DPL.Document(doc)
-    const DPLDirective = new Bot.Directive.DPL.RenderDocument()
+    const document = new Document(doc)
+    const DPLDirective = new RenderDocument()
     DPLDirective.setDocument(document)
     return {
         directives: [DPLDirective],
