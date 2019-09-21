b6f55155c081fb11f2555da1ba9d67c4a4114087
lumotian <lumotian@baidu.com>
2019-09-22 07:17:59 +0800

    aa

--- header.js
+++ header.js
@@ -7,25 +7,26 @@ const header = {
     "height": "40dp",
     "width": "100%",
     "flex-direction": "row",
+    "margin-bottom": '10dp',
     "items": [
         {
             "type": "Text",
             "font-size": "19px",
             "vertical-align": "bottom",
             "text": "智能",
-            "margin-right": "10dp",
+            "margin-right": "30dp",
         }, {
             "type": "Text",
             "font-size": "19px",
             "vertical-align": "bottom",
             "text": "智能",
-            "margin-right": "10dp",
+            "margin-right": "30dp",
         }, {
             "type": "Text",
             "font-size": "19px",
             "vertical-align": "bottom",
             "text": "智能",
-            "margin-right": "10dp",
+            "margin-right": "30dp",
         }
     ]
 }
