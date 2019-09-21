a359fe43919b68c2d4da6f3adb47d080295cc4a8
lumotian <lumotian@baidu.com>
2019-09-21 21:41:14 +0800

    aa

--- render.js
+++ render.js
@@ -52,7 +52,7 @@ const itemTemplate = index => ({
         }, {
             "type": "Text",
             "font-size": "19px",
-            "color": "${data["+index+"].pos ? '#F2564E' : '#17A56B'}",
+            "color": "${data["+index+"].pos ? #F2564E : #17A56B}",
             "text": "${data["+index+"].value}",
             "text-align": "right",
             "position": "relative",
