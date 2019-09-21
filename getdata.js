a52d43f5117b721cd2b463159f365a175d26c144
lumotian <lumotian@baidu.com>
2019-09-21 16:51:14 +0800

    aa

--- getdata.js
+++ getdata.js
@@ -9,12 +9,12 @@ connection.connect();
  
 connection.query('select * from radar', function (error, results, fields) {
   if (error) throw error;
-  console.log(results);
+  console.log(
   results.map(_ => ({
       img: "http://47.94.136.153:8000/fig/"+_.name+".png",
       title: _.name,
       active: _.active,
       value: '%' + (Math.floor(_.value*10000)/100),
       factor: _.factor,
-  }))
+  })))
 });
